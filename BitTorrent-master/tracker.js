067592a3b2d3fb8b2e3a7e21dcf323f5b4fc8f19
Levistpatrick <42749570+Levistpatrick@users.noreply.github.com>
2018-11-28 14:02:28 -0600

    Add files via upload

--- BitTorrent-master/tracker.js
+++ BitTorrent-master/tracker.js
@@ -7,37 +7,15 @@ function Tracker(){
   //implement all six functions below
 function numOfSeeds(list){
   let found = 0;
-  for(let i=0; i<list.length; i++){
-    if(list[i] !== null){
-      found++;
+  for (let i=0; i<list.lenght; i++){
+    if(list [i] !== null){
+      found ++;;
     }
   }
-}
-function addSeed(s){
-  list.push(s);
-  return list;
-}
-function removeSeed(list, name){
-    for(let i=0; i<list.length; i++){
-      if(list[i]===name){
-        return null
-      }
-    }
-  }
-function reportHealth(list){
-    let sum=0;
-    for(let i=0; i<list.length; i++){
-      if(list[i]!=0){
-      sum=sum+list[i];
-      }
-    }
-    return sum/list.length;
-    
-  }
-}
-function update(){
-  Math.random(0, 1);
-  for(let i=0; i<list.length; i++){
-  
-  
-}
+ return found;
+ {
+   function addSeed(s)
+   seeds.push(s)
+ }
+
+
